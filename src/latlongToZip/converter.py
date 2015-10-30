0482b8bbb384d237c89a37cf9e706297da6e9500
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-10-29 20:46:57 -0700

    changes for adding seatgeek data

--- src/latlongToZip/converter.py
+++ src/latlongToZip/converter.py
@@ -3,16 +3,26 @@
 import collections
 from geopy.geocoders import Nominatim
 
-inputfile = file("../../data/taxi_data.csv")
+	inputfile = file("../../data/taxi_data.csv")
 geolocator = Nominatim()
 
-for line in inputfile:
+	for line in inputfile:
 	latitude = line.split(',')[5]
 	longitude = line.split(',')[6]
-	
+
 	print (latitude, longitude)
 	location = geolocator.reverse(latitude[1:-1]+", "+longitude[1:-1])
 	print (location.raw)
+
+	dropoffdate = line.split(',')[2]
+	seatgeekdata = url("http://api.seatgeek.com/2/events?datetime_utc=" + dropoffdate)
+
+	print seatgeekdata
+
+	for event in seatGeekData:	
+		if (event(lat,long) == latitude, longitude):
+			map(event, count) # aggregate social media check in data to find the number of people count
+
 
 #from geopy.geocoders import Nominatim
 #geolocator = Nominatim()
