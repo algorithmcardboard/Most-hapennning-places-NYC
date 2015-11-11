22005071116bf73d1ae8be6a418d0fdafb4f477d
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-11-11 00:31:24 -0500

    using the api endpoint to fetch data for eventful

---
+++ src/eventful/.hpcmodules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load virtualenv/12.1.1
+module load libxml2/intel/2.9.1
+module load libxslt/intel/1.1.28
--- src/eventful/fetch_eventful.py
+++ src/eventful/fetch_eventful.py
@@ -1,3 +1,19 @@
 import dotenv
 import requests
-import requests.auth
+import os
+import sys
+
+dotenv.load_dotenv('.env')
+
+EF_API_KEY = os.environ.get("EF_API_KEY")
+SEARCH_ENDPOINT = "http://api.eventful.com/rest/events/search"
+
+if not EF_API_KEY:
+	print "no key"
+	sys.exit(1)
+
+payload = {'app_key': EF_API_KEY, 'location':'NYC'}
+
+res = requests.get(SEARCH_ENDPOINT, params=payload)
+
+print res.text
