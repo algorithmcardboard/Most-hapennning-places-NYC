7a32829ad772868022efc9c38c8607f7d428c85f
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-11-11 18:33:40 -0500

    adding a sample data folder using the json api

--- src/eventful/fetch_eventful.py
+++ src/eventful/fetch_eventful.py
@@ -6,13 +6,13 @@ import sys
 dotenv.load_dotenv('.env')
 
 EF_API_KEY = os.environ.get("EF_API_KEY")
-SEARCH_ENDPOINT = "http://api.eventful.com/rest/events/search"
+SEARCH_ENDPOINT = "http://api.eventful.com/json/events/search"
 
 if not EF_API_KEY:
 	print "no key"
 	sys.exit(1)
 
-payload = {'app_key': EF_API_KEY, 'location':'NYC'}
+payload = {'app_key': EF_API_KEY, 'location':'NYC', 'page_size':20}
 
 res = requests.get(SEARCH_ENDPOINT, params=payload)
 
