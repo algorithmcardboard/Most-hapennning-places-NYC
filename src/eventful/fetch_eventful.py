a80278223928cfef221cfd171513c49c0ae1ed17
Narasimman <narasi.mit@gmail.com>
2015-11-11 20:13:42 -0500

    adding code to fetch event data page-wise

--- src/eventful/fetch_eventful.py
+++ src/eventful/fetch_eventful.py
@@ -2,6 +2,8 @@ import dotenv
 import requests
 import os
 import sys
+import json
+import time
 
 dotenv.load_dotenv('.env')
 
@@ -12,8 +14,22 @@ if not EF_API_KEY:
 	print "no key"
 	sys.exit(1)
 
-payload = {'app_key': EF_API_KEY, 'location':'NYC', 'page_size':20}
+payload = {'app_key': EF_API_KEY, 'location':'NYC', 'date': 'Past', 'page_size' : 20}
 
 res = requests.get(SEARCH_ENDPOINT, params=payload)
 
-print res.text
+req_count = json.loads(res.text.encode("utf-8"))
+
+count = req_count['page_count']
+
+print count
+
+for i in range(5):
+	event_payload = {'app_key': EF_API_KEY, 'location':'NYC', 'date': 'Past', 'page_number': i + 1, 'page_size' : 20}
+	res = requests.get(SEARCH_ENDPOINT, params=event_payload)
+
+	with open(os.path.join("output/", str(i+1)), "w") as file:
+		file.write(res.text.encode("utf-8"))
+
+	time.sleep(4)
+#print res.text.encode("utf-8")
